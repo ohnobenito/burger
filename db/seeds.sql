c0abf24237d95a7a297ec4406fc148c62de35d1c
Benito Gonzalez <ohnobenito@gmail.com>
2021-01-07 16:23:19 -0600

    added seeds file to fill in burger db

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name) VALUES ('Hamburger');
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Impossible Whopper');
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Juicy Lucy', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', true);
+
